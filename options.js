216bf10cb219ca1e4e39d91b850cccd980a35c1a
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-05-03 03:20:27 -0400

    Update options.js

--- options.js
+++ options.js
@@ -1,20 +1,34 @@
-let page = document.getElementbyID("buttonDiv");
+let page = document.getElementById("buttonDiv");
 
-const ButtonCombos = [
-	{work: 25, break: 5}, 
-	{work: 45, break: 15},
-	{work: 30, break: 15},
-	{work: 60, break: 15}
-];
+let btn1 =  document.getElementById("btn1");
+let btn2 =  document.getElementById("btn2");
+let btn3 =  document.getElementById("btn3");
+let btn4 =  document.getElementById("btn4");
 
-function constructOps(combosList) {
-	for (let item of combosList) {
-		let button = document.createElement('button');
-		button.innerHTML = "Work: " + item["work"] + " Break: " + item["break"];
-		button.addEventListener('click', function() {
-        chrome.storage.sync.set({work: item["work"], break: item["break"]}, function() {
-          console.log("Work: " + item["work"] + " Break: " + item["break"]);
-        })
-      });
-	}
-}
+
+btn1.addEventListener('click', function() {
+	console.log("Work: 25, Break: 5");
+	chrome.storage.sync.set({work: 25, break: 5}, function() {
+  		console.log("Work: 25, Break: 5");
+	})
+});
+
+btn2.addEventListener('click', function() {
+	chrome.storage.sync.set({work: 45, break: 15}, function() {
+  		console.log("Work: 45, Break: 15");
+	})
+});
+
+btn3.addEventListener('click', function() {
+	chrome.storage.sync.set({work: 30, break: 15}, function() {
+  		console.log("Work: 30, Break: 15");
+	})
+});
+
+btn4.addEventListener('click', function() {
+	chrome.storage.sync.set({work: 60, break: 15}, function() {
+  		console.log("Work: 60, Break: 15");
+	})
+});
+		
+
